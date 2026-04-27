205e0f456238fcfc9ad1b8cddf701799e11b6be0
ibrohimjonodilov0011-cpu <ibrohimjonodilov0011@icloud.com>
2026-04-27 21:53:56 +0500

    Update spotify.js

--- api/spotify.js
+++ api/spotify.js
@@ -8,11 +8,19 @@ export default async function handler(req, res) {
 
     const client_id = process.env.CLIENT_ID;
     const client_secret = process.env.CLIENT_SECRET;
+    if (!client_id || !client_secret) {
+    return res.status(500).json({ error: "no env" });
+}
 
     const tokenRes = await fetch("https://accounts.spotify.com/api/token", {
         method: "POST",
         headers: {
-            "Authorization": "Basic " + Buffer.from(client_id + ":" + client_secret).toString("base64"),
+            const basic = btoa(client_id + ":" + client_secret);
+
+headers: {
+    "Authorization": "Basic " + basic,
+    "Content-Type": "application/x-www-form-urlencoded"
+}
             "Content-Type": "application/x-www-form-urlencoded"
         },
         body: "grant_type=client_credentials"
